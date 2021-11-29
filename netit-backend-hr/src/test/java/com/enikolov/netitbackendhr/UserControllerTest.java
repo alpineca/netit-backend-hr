b2d8633497c1f1a021bc56122d1f65c8c561f3b4
Emil Nikolov <e.nikolov92@gmail.com>
2021-11-29 22:09:43 +0200

    Simple employer registration done

---
+++ netit-backend-hr/src/test/java/com/enikolov/netitbackendhr/UserControllerTest.java
@@ -0,0 +1,12 @@
+package com.enikolov.netitbackendhr;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class TestUserController {
+
+    @Autowired
+
+
+}
