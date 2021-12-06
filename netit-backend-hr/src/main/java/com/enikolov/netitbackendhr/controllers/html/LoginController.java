c3d82535fd27ef8feab1c8236e5ef16bc68a9445
Emil Nikolov <e.nikolov92@gmail.com>
2021-12-06 11:04:01 +0200

    Login warning added

--- netit-backend-hr/src/main/java/com/enikolov/netitbackendhr/controllers/html/LoginController.java
+++ netit-backend-hr/src/main/java/com/enikolov/netitbackendhr/controllers/html/LoginController.java
@@ -2,6 +2,8 @@ package com.enikolov.netitbackendhr.controllers.html;
 
 import java.util.Optional;
 
+import com.enikolov.netitbackendhr.components.InfoMessage;
+import com.enikolov.netitbackendhr.enums.MessageStyle;
 import com.enikolov.netitbackendhr.models.users.User;
 import com.enikolov.netitbackendhr.repositories.users.UserRepository;
 
@@ -11,6 +13,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 import org.springframework.web.servlet.view.RedirectView;
 
 @Controller
@@ -23,12 +26,21 @@ public class LoginController {
     public String getLoginPage(Model model){
 
 
+        // if(message != null){
+        //     model.addAttribute("message", message);
+        // }
+
+        InfoMessage message = new InfoMessage();
+        message.setMessage("Username or password not valid!");
+        message.setStyle(MessageStyle.ERROR_MSG);
+
         model.addAttribute("user", new User());
+        model.addAttribute("message", message);
         return "auth/login";
     }
 
     @PostMapping("/login")
-    public RedirectView loginProcess(@ModelAttribute User user, Model model){
+    public RedirectView loginProcess(@ModelAttribute User user, Model model, RedirectAttributes redirectAttributes){
 
         Optional<User> findUser = this.userRepository.findUserByUsernameAndPassword(user.getUsername(), user.getPassword());
 
@@ -49,14 +61,12 @@ public class LoginController {
                 return new RedirectView("super-dashboard");
             }
         }
+        InfoMessage message = new InfoMessage();
+        message.setMessage("Username or password not valid!");
+        message.setStyle(MessageStyle.ERROR_MSG);
 
+
+        redirectAttributes.addFlashAttribute(message);
         return new RedirectView("login");
     }
-
-//    @PostMapping("/logout")
-//    public RedirectView getAfterLogoutPage(){
-//
-//        System.out.println("Logged out.... maybe");
-//        return new RedirectView("index");
-//    }
 }
